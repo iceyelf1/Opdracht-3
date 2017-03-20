6ad53ccd024254eb267d3b4aef29f3d6312b950f
iceyelf1 <josha-wesselink@hotmail.com>
2017-03-20 13:31:33 +0100

    Opdracht 3
    Opdracht 3

---
+++ Kalender/Kalender/KalenderWebForm.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="KalenderWebForm.aspx.cs" Inherits="Kalender.KalenderWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Calendar ID="KalVerjaardag" runat="server" OnSelectionChanged="KalVerjaardag_SelectionChanged"></asp:Calendar>
+        <br />
+        U heeft deze datum geselecteerd:<br />
+        <asp:TextBox ID="tbDatum" runat="server" Width="300px"></asp:TextBox>
+        <br />
+        <br />
+        U bent nu:<p>
+            <asp:TextBox ID="tbLeeftijd" runat="server" OnTextChanged="TextBox2_TextChanged" Width="300px"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Kalender/Kalender/KalenderWebForm.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Kalender
+{
+    public partial class KalenderWebForm : System.Web.UI.Page
+    {
+        private DateTime verjaardag;
+        private string verjaardagText = "Mijn geboortedatum is op: ";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                verjaardag = new DateTime(1995, 1, 1);
+                KalVerjaardag.VisibleDate = verjaardag;
+            }
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void KalVerjaardag_SelectionChanged(object sender, EventArgs e)
+        {
+            verjaardag = KalVerjaardag.SelectedDate;
+            tbDatum.Text = verjaardagText + verjaardag.ToLongDateString();
+
+            DateTime now = DateTime.Now;
+
+            TimeSpan difference = now.Subtract(verjaardag);
+            int jaren = difference.Days / 365;
+
+            tbLeeftijd.Text = jaren.ToString();
+        }
+    }
+}
